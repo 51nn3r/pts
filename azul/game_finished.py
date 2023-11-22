b033400cd8e2894b75f11540596b1594eb1de869
BritishTeapot <f.kovalyoff@gmail.com>
2023-11-23 00:20:56 +0100

    Fixed GameFinished problem

--- azul/game_finished.py
+++ azul/game_finished.py
@@ -1,10 +1,14 @@
 from typing import List, Optional
 from simple_types import Tile,FinishRoundResult
 class GameFinished:
-    def gameFinished(wall: List[List[Optional[Tile]]])->FinishRoundResult:
+    def gameFinished(self, wall: List[List[Optional[Tile]]]) -> FinishRoundResult:
+        r: FinishRoundResult = FinishRoundResult("N")
         for i in wall:
+            is_line: bool = True
             for j in i:
-                if j == None: return FinishRoundResult("N")
-        
-        return FinishRoundResult("F")
+                if j is None:
+                    is_line = False
+                    break
+            if is_line:
+                return FinishRoundResult("F")
  
