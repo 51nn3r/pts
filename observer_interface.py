9ae0ca4ba7b0dfc5476fe4e7ecb5e35a3f5452cc
51nn3r <78556003+51nn3r@users.noreply.github.com>
2023-11-29 04:39:05 +0300

    Delete observer_interface.py

--- observer_interface.py
+++
@@ -1,9 +0,0 @@
-from azul.interfaces import ObserverInterface as ObserverSimpleInterface
-
-
-class ObserverInterface(ObserverSimpleInterface):
-    def notify(
-            self,
-            new_state: str,
-    ) -> None:
-        print(new_state)
